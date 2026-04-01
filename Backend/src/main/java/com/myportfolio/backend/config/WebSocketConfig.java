a173973d58ebf6ae4a2bd81dc430eab3b3ac0b22
OmarT94 <93581484+OmarT94@users.noreply.github.com>
2026-04-01 22:36:22 +0200

    add config (Security, WebSocket)Und ExceptionHandler

---
+++ Backend/src/main/java/com/myportfolio/backend/config/WebSocketConfig.java
@@ -0,0 +1,28 @@
+package com.myportfolio.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // الـ Admin يستقبل الإشعارات من هذا الـ prefix
+        registry.enableSimpleBroker("/topic");
+        // الـ Frontend يرسل للـ Backend من هذا الـ prefix
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // نقطة الاتصال بالـ WebSocket
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("http://localhost:3000")
+                .withSockJS(); // دعم المتصفحات القديمة
+    }
+}
